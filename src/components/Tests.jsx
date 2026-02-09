25591a301cb44cf5a1cfbcf9fd06a5d93d50008a
Muskan Chaurasia <122660165+Muskan2512@users.noreply.github.com>
2026-02-09 08:34:25 +0530

    Update test type

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -5,7 +5,7 @@ const Tests = () => {
   return (
     <div className=' text-gray-500 mx-auto flex items-center p-10 gap-5 '>
       <Cards testType="Sample Test" content="This is a sample test designed to help you get familiar with the interface. Password: password"/>
-      <Cards testType="INFOTREK'25" content="This is the official test for ACUMEN'25. The password to unlock it will be shared shortly."/>
+      <Cards testType="ACUMEN'26" content="This is the official test for ACUMEN'26. The password to unlock it will be shared shortly."/>
     </div>
   )
 }
