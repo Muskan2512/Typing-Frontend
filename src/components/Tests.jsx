381a109f431e5106de8e2a0e2f7a13e70511c6a9
Muskan Chaurasia <122660165+Muskan2512@users.noreply.github.com>
2026-02-09 08:30:26 +0530

    Update test content and passwords in Tests component

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -4,8 +4,8 @@ import Cards from './Cards'
 const Tests = () => {
   return (
     <div className=' text-gray-500 mx-auto flex items-center p-10 gap-5 '>
-      <Cards testType="Sample Test" content="This is a sample test designed to help you get familiar with the interface. Password: sample"/>
-      <Cards testType="INFOTREK'25" content="This is the official test for INFOTREK'25. The password to unlock it will be shared shortly."/>
+      <Cards testType="Sample Test" content="This is a sample test designed to help you get familiar with the interface. Password: password"/>
+      <Cards testType="INFOTREK'25" content="This is the official test for ACUMEN'25. The password to unlock it will be shared shortly."/>
     </div>
   )
 }
